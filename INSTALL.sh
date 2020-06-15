2800771a6855b1245231fe8b42ecc20c7fe643f6
Milan Munzar <mimunzar@gmail.com>
2020-06-15 15:57:18 +0200

    Adds configuration for I3

--- INSTALL.sh
+++ INSTALL.sh
@@ -4,5 +4,9 @@ for f ("Xresources" "xinitrc" "zprofile" "i3status.conf"); do
     ln -svf ~/.XConf/$f ~/.$f
 done
 
+[ -d ~/.config ] || mkdir ~/.config
 ln -svf ~/.XConf/redshift.conf ~/.config/
+
+[ -d ~/.config/i3 ] || mkdir ~/.config/i3
+ln -svf ~/.XConf/i3config ~/.config/i3/config
 
